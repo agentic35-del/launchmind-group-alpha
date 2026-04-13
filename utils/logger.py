3b2d22feed44f7e001c141e48eccf8d0eb700493
agentic35-del <agentic35@gmail.com>
2026-04-13 17:19:37 +0500

    Delete utils directory

--- utils/logger.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-
-def log(agent: str, message: str) -> None:
-    now = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[{now}] [{agent.upper()}] {message}")
